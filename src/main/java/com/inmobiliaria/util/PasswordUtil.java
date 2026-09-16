c560bd46b0d8e188552b699e3d31a435d8c5828d
Santi-lizcano <santiagolizcano28@gmail.com>
2026-09-16 16:27:09 -0500

    Add PasswordUtil for hashing and verifying passwords

---
+++ src/main/java/com/inmobiliaria/util/PasswordUtil.java
@@ -0,0 +1,11 @@
+package com.inmobiliaria.util;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+    public static String hash(String plain) {
+        return BCrypt.hashpw(plain, BCrypt.gensalt(10));
+    }
+    public static boolean verify(String plain, String hash) {
+        return BCrypt.checkpw(plain, hash);
+    }
+}
